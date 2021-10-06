99ea38429f57c41972b1ff045117b1d036bd334e
sujin <gusu@likelion.net>
2021-10-06 14:49:59 +0900

    add: navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.css';
 
 function Navigation() {
     return (
-        <div>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
+        <div className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
         </div>
     )
 }
